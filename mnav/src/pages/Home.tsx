832d81d39254686d44f5bd8f61aa92ba7da5336b
Vyom Thakkar <vyomthakkar@berkeley.edu>
2019-11-02 14:46:19 -0700

    add map

--- mnav/src/pages/Home.tsx
+++ mnav/src/pages/Home.tsx
@@ -1,12 +1,22 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+import Button from '@material-ui/core/Button';
+// import Speech from 'react-speech';
+import GoogleMapReact from 'google-map-react';
 
 const Home: React.FC = () => {
+
+  const center = {
+    lat: 37.8715,
+    lng: -122.2730
+  }
+
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
+          <IonTitle>Marina Navigator</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
@@ -19,6 +29,16 @@ const Home: React.FC = () => {
           will be your guide.
         </p>
       </IonContent>
+      <IonContent>
+        <GoogleMapReact bootstrapURLKeys={{key: 'AIzaSyAcnohmySzY0Me649IBXFQok6W-VzLJUDM'}}
+        defaultCenter={center}
+        defaultZoom={11}
+        >
+        </GoogleMapReact>
+      </IonContent>
+      <Button variant="contained" color="secondary">
+      Navigate Me
+      </Button>
     </IonPage>
   );
 };

9bef3d4509ad016fe4e7d521e9a9fcc3cdde08e8
Vyom Thakkar <vyomthakkar@berkeley.edu>
2019-11-21 19:38:20 -0800

    add locations

--- mnav/src/pages/Home.tsx
+++ mnav/src/pages/Home.tsx
@@ -1,21 +1,46 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import React, {useState} from 'react';
 import Button from '@material-ui/core/Button';
 // import Speech from 'react-speech';
 import GoogleMapReact from 'google-map-react';
+import Geocode from 'react-geocode';
 
 const Home: React.FC = () => {
 
-  const center = {
-    lat: 37.8715,
-    lng: -122.2730
-  }
+  const [latDest, setLat] = useState("...");
+  const [lngDest, setLng] = useState("");
 
+  const ApiKey = "AIzaSyAcnohmySzY0Me649IBXFQok6W-VzLJUDM";
+  Geocode.setApiKey(ApiKey);
+  Geocode.setLanguage("en");
+  Geocode.enableDebug();
+  const center = {
+    lat: 37.779835,
+    lng: -122.387681
+  };
+
+  const setPoints = {
+    "muni stop" : "QJH5+QX SoMa, San Francisco, CA",
+    "java house" : "QJJ6+HV SoMa, San Francisco, CA",
+    "yacht club" : "QJH6+XR SoMa, San Francisco, CA",
+    "pier forty building" : "QJJ6+QR SoMa, San Francisco, CA",
+    "bike hut": "QJJ6+MW SoMa, San Francisco, CA",
+    "city kayak" : "QJJ7+MF SoMa, San Francisco, CA"
+  };
+
+  const input = "city kAyak"
+
+  Geocode.fromAddress(setPoints[input.toLowerCase()]).then(
+    response => {
+      setLat(response.results[0].geometry.location.lat);
+      setLng(response.results[0].geometry.location.lng);
+    }
+  )
 
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
+        <IonToolbar color = "secondary">
           <IonTitle>Marina Navigator</IonTitle>
         </IonToolbar>
       </IonHeader>
@@ -29,16 +54,21 @@ const Home: React.FC = () => {
           will be your guide.
         </p>
       </IonContent>
-      <IonContent>
-        <GoogleMapReact bootstrapURLKeys={{key: 'AIzaSyAcnohmySzY0Me649IBXFQok6W-VzLJUDM'}}
+      <IonContent className="ion-text-center">
+        Destination: {latDest} , {lngDest}
+      </IonContent>
+      <IonContent className="ion-padding">
+        <GoogleMapReact bootstrapURLKeys={{key: ApiKey}}
         defaultCenter={center}
         defaultZoom={11}
         >
         </GoogleMapReact>
       </IonContent>
-      <Button variant="contained" color="secondary">
-      Navigate Me
-      </Button>
+      <IonContent className="ion-text-center">
+        <Button variant="contained" color="secondary">
+        Navigate Me
+        </Button>
+      </IonContent>
     </IonPage>
   );
 };
